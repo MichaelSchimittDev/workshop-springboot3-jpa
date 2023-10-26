78b505b413102c4af02a9c2fb297419cb825f5f5
Michael Starrk Schimitt <michaelschimittpro@gmail.com>
2023-10-26 13:24:46 -0300

    Service layer, component registration

--- src/main/java/com/stkdev/course/entities/Order.java
+++ src/main/java/com/stkdev/course/entities/Order.java
@@ -1,15 +1,16 @@
 package com.stkdev.course.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,31 +20,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Table(name = "tb-user")
-
-public class User implements Serializable{
+@Table(name = "tb-order")
+public class Order implements Serializable{
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String name;
-	private String email;
-	private String phone;
-	private String password;
+	private Instant moment;
 	
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
-
-	public User(Long id, String name, String email, String phone, String password) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		this.password = password;
-	}
-	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
 }
