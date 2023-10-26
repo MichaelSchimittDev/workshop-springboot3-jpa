4616ca76992ff7568d73402b2979c0548a1efc13
Michael Starrk Schimitt <michaelschimittpro@gmail.com>
2023-10-26 09:40:34 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/stkdev/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.stkdev.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.stkdev.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+	
+}
